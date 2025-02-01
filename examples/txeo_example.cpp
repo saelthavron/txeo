3b75d7cfe39b741b6526f5e8b354e15a8769b6be
rdabra <robertodias70@outlook.com>
2025-01-31 21:05:28 -0300

    Implementing cpp/tf type conversions

--- examples/txeo_example.cpp
+++ examples/txeo_example.cpp
@@ -1,17 +1,60 @@
-#include "txeo/Tensor.h"
+#include "txeo/detail/utils.h"
 #include <iostream>
-#include <string>
-#include <vector>
+
+#include <tensorflow/core/framework/tensor.h>
+#include <tensorflow/core/framework/types.pb.h>
+
+void create_tensor() {
+  tf::Tensor S(tf::DT_DOUBLE, tf::TensorShape({}));
+  auto scal = S.scalar<double>();
+  scal() = 10;
+  std::cout << "Scalar: " << scal << std::endl;
+  std::cout << "Scalar dimensions: " << scal.NumDimensions << std::endl;
+
+  tf::Tensor V(tf::DT_DOUBLE, tf::TensorShape({3}));
+  auto vec = V.vec<double>();
+  vec(0) = 1;
+  vec(1) = 2;
+  vec(2) = 3;
+  std::cout << "Vector: " << vec << std::endl;
+  std::cout << "Vector dimensions: " << vec.NumDimensions << std::endl;
+
+  tf::Tensor M(tf::DT_DOUBLE, tf::TensorShape({3, 3}));
+  auto mat = M.matrix<double>();
+  mat(0, 0) = 1.0;
+  mat(1, 0) = 2.0;
+  mat(2, 0) = 3.0;
+  mat(0, 1) = 4.0;
+  mat(1, 1) = 5.0;
+  mat(2, 1) = 6.0;
+  mat(0, 2) = 7.0;
+  mat(1, 2) = 8.0;
+  mat(2, 2) = 9.0;
+  std::cout << "Matrix: \n" << mat << std::endl;
+  std::cout << "Matrix dimensions: " << mat.NumDimensions << std::endl;
+
+  tf::Tensor M1(tf::DT_DOUBLE, tf::TensorShape({2, 3}));
+  auto mat1 = M1.matrix<double>();
+  mat1.setZero();
+  std::cout << "Zero Matrix: \n" << mat1 << std::endl;
+
+  tf::Tensor M2(tf::DT_DOUBLE, tf::TensorShape({4, 2}));
+  auto mat2 = M2.matrix<double>();
+  mat2.setConstant(2.6);
+  std::cout << "Value Matrix: \n" << mat2 << std::endl;
+}
 
 int main() {
 
-  std::vector<std::string> vec{"roberto", "dias", "algarte"};
+  tf::Tensor S(txeo::detail::get_tf_dtype<double>(), tf::TensorShape({}));
+  auto scal = S.scalar<double>();
+  scal() = 10;
+  std::cout << "Scalar: " << scal << std::endl;
+  std::cout << "Scalar dimensions: " << scal.NumDimensions << std::endl;
 
-  int a = 2;
+  txeo::detail::cpp_type<tf::DT_DOUBLE> number = 100.2;
 
-  Tensor p{"roberto", "algarte"};
-
-  std::cout << p.nome_completo() << std::endl;
+  std::cout << "Scalar: " << number << std::endl;
 
   return 0;
 }
---
+++ include/txeo/detail/utils.h
@@ -0,0 +1,34 @@
+#ifndef TXEO_UTILS_H
+#define TXEO_UTILS_H
+#pragma once
+
+#include "tensorflow/core/framework/types.h"
+
+namespace tf = tensorflow;
+
+namespace txeo::detail {
+
+/**
+ * @brief Gets the correspondent TensorFlow type from the cpp type
+ *
+ * @tparam T cpp type
+ * @return tf::DataType TensorFlow type
+ */
+template <typename T>
+constexpr tf::DataType get_tf_dtype() {
+  static_assert(tf::DataTypeToEnum<T>::value != tf::DT_INVALID,
+                "Unsupported C++ type for TensorFlow mapping");
+  return tf::DataTypeToEnum<T>::value;
+}
+
+/**
+ * @brief Specifies the cpp type from the TensorFlow type
+ *
+ * @tparam T TensorFlow type
+ */
+template <tf::DataType T>
+using cpp_type = typename tf::EnumToDataType<T>::Type;
+
+} // namespace txeo::detail
+
+#endif
