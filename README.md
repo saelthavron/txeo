586424543056b7d6c2cd0b525fbae7a490d8c764
rdabra <robertodias70@outlook.com>
2025-02-17 18:03:47 -0300

    Elaborating docs for first publication.

--- README.md
+++ README.md
@@ -1,23 +1,118 @@
-# Txeo: A Developer-Friendly TensorFlow C++ Wrapper
+# Txeo: a Developer-Friendly TensorFlow C++ Wrapper
 
 [![License: Apache 2.0](https://img.shields.io/badge/license-Apache%202.0-blue.svg)](LICENSE)
 
 ## 📝 Overview
 
-**Txeo** is an intuitive **C++ wrapper for TensorFlow**, designed to simplify the TensorFlow C++ API while maintaining **high performance and flexibility**. It enables developers to work with TensorFlow in C++ as easily as in Python, without the steep learning curve of TensorFlow’s low-level C++ API.
+**Txeo** is an intuitive and light **C++ wrapper for TensorFlow 2.0**, designed to simplify the TensorFlow C++ API while maintaining **high performance and flexibility**. Developed purely in Modern C++, **Txeo** enables developers to work with TensorFlow as easily as in Python, without the steep learning curve of TensorFlow’s low-level C++ API.
 
 ## ✨ Features
 
 - 📦 **Easy-to-use API** – Designed for a clean and readable C++ interface.
 - 🔧 **Tensor abstraction** – Simplifies TensorFlow tensor creation and manipulation.
-- 🏗 **Model wrapper** – Streamlined model loading and inference.
-- 🚀 **Performance-optimized** – Supports GPU acceleration with CUDA.
-- 🔍 **GoogleTest Integration** – Built-in testing framework.
+- 💾 **Tensor IO** – Reading and writing tensors from and to text files.
+- 🏗 **Model wrapper** – Streamlined saved model loading and inference.
+- 💻 **XLA Enabling** - Enabling/disabling of XLA features.
 
 ## ⚡ Installation
 
-### **1️⃣ Clone the Repository**
+### **Requirements**
+
+- **Build Essentials** - Essential tools for c/c++ development.
+- **Compilers** – clang v19+ or gcc v13+.
+- **TensorFlow 2.18.0** - <https://github.com/tensorflow/tensorflow>
+- **Protobuf 3.21.9** - <https://github.com/protocolbuffers/protobuf>
+
+### **Steps with TensorFlow and Protobuf binaries (fastest way)**
+
+#### **1️⃣ Download and install binaries**
+
+Choosing clang generated binaries for protobuf and installing them in `/opt`:
 
 ```sh
-git clone https://github.com/rdabra/rhoe-tf-wrapper.git
-cd rhoe-tf-wrapper
+wget https://github.com/rdabra/txeo-tf/releases/download/v1.0.0/libprotobuf-3.21.9-clang19-linux-x64.tar.gz
+sudo tar -xzf libprotobuf-3.21.9-clang19-linux-x64.tar.gz -C /opt/
+export Protobuf_ROOT_DIR=/opt/protobuf
+```
+
+Choosing clang generated binaries for tensorflow and installing them in `/opt`:
+
+```sh
+wget https://github.com/rdabra/txeo-tf/releases/download/v1.0.0/libtensorflow-2.18-clang19-linux-x64-cpu.tar.gz
+sudo tar -xzf libtensorflow-2.18-clang19-linux-x64-cpu.tar.gz -C /opt/
+export TensorFlow_ROOT_DIR=/opt/tensorflow
+```
+
+> **📌 Important Note:** The source codes of TensorFlow and Protobuf used for the binaries available here for download were not modified in any way, shape or form. Our intention is solely to provide the user a fast way to install **Txeo**.  
+
+📁 Other assets are available <https://github.com/rdabra/txeo-tf/releases/tag/v1.0.0>
+
+#### **2️⃣ Clone and install Txeo**
+
+```sh
+git clone https://github.com/rdabra/txeo-tf.git
+cd txeo-tf
+mkdir build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j$(nproc)
+sudo make install
+```
+
+### **Steps with TensorFlow and Protobuf built from source (may take a very long time)**
+
+#### **1️⃣ Clone and install Protobuf**
+
+```sh
+git clone https://github.com/protocolbuffers/protobuf.git
+cd protobuf
+git checkout refs/tags/v3.21.9
+cmake -S. -Bcmake-out -G Ninja -DCMAKE_INSTALL_PREFIX="/opt/protobuf" -Dprotobuf_ABSL_PROVIDER=package -Dprotobuf_BUILD_TESTS=OFF
+cd cmake-out
+cmake --build .
+sudo cmake --install .
+export Protobuf_ROOT_DIR=/opt/protobuf 
+```
+
+#### **2️⃣ Clone and install Tensorflow**
+
+Bazel must be installed previously (see <https://github.com/bazelbuild/bazelisk>). During the config phase, choose not to compile for gpu (without cuda support). This build was tested in clang v19 and gcc v13 (without the `-std=gnu2x` key).
+
+```sh
+git clone https://github.com/tensorflow/tensorflow.git
+cd tensorflow
+git checkout refs/tags/v2.18.0
+./configure
+bazel build -c opt --copt=-std=gnu2x --copt=-mavx --copt=-mavx2 --copt=-mfma --copt=-msse4.1 --copt=-msse4.2 //tensorflow:libtensorflow_cc.so //tensorflow:libtensorflow_framework.so //tensorflow:install_headers
+```
+
+Copy generated libraries and includes to `/opt`
+
+```sh
+cd bazel-bin
+sudo mkdir /opt/tensorflow
+sudo cp -r tensorflow/include /opt/tensorflow
+sudo mkdir /opt/tensorflow/lib
+sudo cp -r tensorflow/*.so* /opt/tensorflow/lib
+export TensorFlow_ROOT_DIR=/opt/tensorflow 
+```
+
+#### **3️⃣ Clone and install Txeo**
+
+```sh
+git clone https://github.com/rdabra/txeo-tf.git
+cd txeo-tf
+mkdir build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j$(nproc)
+sudo make install
+```
+
+## 📜 License
+
+Txeo is licensed under the Apache License 2.0.
+
+### Third-Party Licenses
+
+Txeo uses TensorFlow C++ (licensed under Apache 2.0).  
+TensorFlow’s original license is included in [`third_party/tensorflow/LICENSE`](third_party/tensorflow/LICENSE).  
+For more details, visit [TensorFlow GitHub](https://github.com/tensorflow/tensorflow).
--- examples/txeo_example.cpp
+++ examples/txeo_example.cpp
@@ -1,160 +1,17 @@
+
+#include "txeo/Tensor.h"
 #include "txeo/TensorIO.h"
 #include <iostream>
-#include <tensorflow/cc/client/client_session.h>
-#include <tensorflow/cc/saved_model/loader.h>
-#include <tensorflow/cc/saved_model/tag_constants.h>
-#include <tensorflow/core/framework/tensor.h>
-#include <tensorflow/core/framework/tensor_shape.h>
-#include <tensorflow/core/framework/types.pb.h>
-#include <tensorflow/core/protobuf/meta_graph.pb.h>
-#include <tensorflow/core/public/session.h>
-#include <utility>
-
-namespace tf = tensorflow;
-
-#include <iostream>
-#include <tensorflow/cc/saved_model/loader.h>
-#include <tensorflow/core/framework/tensor.h>
-#include <tensorflow/core/public/session.h>
-
-#include "txeo/Predictor.h"
 
 int main() {
 
-  txeo::Tensor<float> xx({1, 256, 256, 3});
+  txeo::Tensor<double> tensor({3, 3}, {1, 2, 3, 4, 5, 6, 7, 8, 9});
 
-  txeo::Predictor pred{"/home/roberto/my_works/personal/hello_python/model_regression"};
-  pred.enable_xla(true);
+  txeo::TensorIO::write_textfile(tensor, "tensor.txt");
 
-  auto devices = pred.get_devices();
-  for (const auto &device : devices) {
-    std::cout << "Name:   " << device.name << " (" << device.device_type << ")" << " - "
-              << device.memory_limit << std::endl;
-  }
+  auto loaded_tensor = txeo::TensorIO::read_textfile<double>("tensor.txt");
 
-  const auto &in_meta = pred.get_input_metadata();
-  const auto &out_meta = pred.get_output_metadata();
-
-  std::cout << "INPUTS:" << std::endl;
-  for (auto &item : in_meta) {
-    std::cout << item.first << ":" << std::endl;
-    std::cout << item.second << ":" << std::endl;
-  }
-
-  std::cout << std::endl;
-  std::cout << "OUTPUTS:" << std::endl;
-  for (auto &item : out_meta) {
-    std::cout << item.first << ":" << std::endl;
-    std::cout << item.second << ":" << std::endl;
-  }
-
-  txeo::TensorIO io{"/home/roberto/my_works/personal/txeo-tf/tests/test_data/teste.txt"};
-  txeo::Tensor<float> input = io.read_text_file<float>(true);
-
-  //  std::vector<std::pair<std::string, txeo::Tensor<float>>> inputs;
-
-  //  inputs.emplace_back("serving_default_dense_8_input:0", input);
-
-  auto resp = pred.predict(input);
-
-  txeo::TensorIO::write_textfile(
-      resp, "/home/roberto/my_works/personal/txeo-tf/tests/test_data/w_teste.txt");
-
-  // tf::SavedModelBundle model;
-  // tf::SessionOptions session_options;
-  // tf::RunOptions run_options;
-  // std::unordered_set<std::string> tags{static_cast<const char *>(tf::kSavedModelTagServe)};
-
-  // std::string model_dir = "/home/roberto/my_works/personal/hello_python/model_regression";
-  // tf::Status status = tf::LoadSavedModel(session_options, run_options, model_dir, tags,
-  // &model); if (!status.ok()) {
-  //   std::cerr << "Error loading model: " << status.ToString() << std::endl;
-  //   return 1;
-  // }
-  // std::cout << "Model loaded successfully!" << std::endl;
-
-  // tensorflow::MetaGraphDef meta_graph_def = model.meta_graph_def;
-  // tensorflow::SignatureDef signature_map =
-  // meta_graph_def.signature_def().at("serving_default");
-
-  // // const auto &input_name = (*std::begin(signature_map.inputs())).second.name();
-
-  // std::cout << "Inputs:\n";
-  // for (const auto &input : signature_map.inputs()) {
-  //   std::cout << input.second.has_name() << std::endl;
-  // }
-
-  // std::cout << std::endl;
-  // std::cout << "Outputs:\n";
-  // for (const auto &output : signature_map.outputs()) {
-  //   std::cout << output.second.name() << std::endl;
-  // }
-
-  // // Prepare input tensor
-  // std::vector<float> input_data = {0.5869565217391305f,
-  //                                  0.2479149852031207f,
-  //                                  0.4f,
-  //                                  1.0f,
-  //                                  0.0f,
-  //                                  1.0f,
-  //                                  0.0f,
-  //                                  1.0f,
-  //                                  0.0f,
-  //                                  0.0f,
-  //                                  0.0f,
-  //                                  0.3913043478260869f,
-  //                                  0.3782620392789884f,
-  //                                  0.0f,
-  //                                  1.0f,
-  //                                  0.0f,
-  //                                  1.0f,
-  //                                  0.0f,
-  //                                  0.0f,
-  //                                  1.0f,
-  //                                  0.0f,
-  //                                  0.0f,
-  //                                  1.0f,
-  //                                  0.2939198278181328f,
-  //                                  0.0f,
-  //                                  1.0f,
-  //                                  0.0f,
-  //                                  0.0f,
-  //                                  1.0f,
-  //                                  0.0f,
-  //                                  1.0f,
-  //                                  0.0f,
-  //                                  0.0f};
-
-  // //
-
-  // tf::Tensor input_tensor(tf::DT_FLOAT, tf::TensorShape({3, 11}));
-  // auto input_map = input_tensor.flat<float>();
-  // for (size_t i = 0; i < input_data.size(); ++i) {
-  //   input_map(i) = input_data[i];
-  // }
-
-  // // Run model inference
-  // std::vector<std::pair<std::string, tf::Tensor>> inputs = {{input_name, input_tensor}};
-  // std::vector<tf::Tensor> outputs;
-
-  // status = model.session->Run(inputs, {output_name}, {}, &outputs);
-  // if (!status.ok()) {
-  //   std::cerr << "Error running model: " << status.ToString() << std::endl;
-  //   return 1;
-  // }
-
-  // // Extract and print predictions
-  // auto output_map = outputs[0].flat<float>();
-  // std::cout << "Model Prediction: ";
-  // for (int i = 0; i < output_map.size(); i++) {
-  //   std::cout << output_map(i) << " ";
-  // }
-  // std::cout << std::endl;
-
-  // if (!model.session->Close().ok()) {
-  //   std::cerr << "Error closing session: " << status.ToString() << std::endl;
-  //   return 1;
-  // }
+  std::cout << loaded_tensor << std::endl;
 
   return 0;
 }
