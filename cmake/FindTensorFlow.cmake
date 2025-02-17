4d2c3bec2d47f9dc158a314b08a97b305b79f48c
rdabra <robertodias70@outlook.com>
2025-02-16 22:48:32 -0300

    Elaborating docs for first publication.

--- cmake/FindTensorFlow.cmake
+++ cmake/FindTensorFlow.cmake
@@ -5,43 +5,61 @@ include(FindPackageHandleStandardArgs)
 find_path(TensorFlow_INCLUDE_DIR
   NAMES tensorflow/core/public/session.h
   HINTS
-    ${TensorFlow_HOME}/include
+    ${TensorFlow_ROOT_DIR}/include  
+    ${TensorFlow_HOME}/include      
+    $ENV{TensorFlow_ROOT_DIR}/include
     $ENV{TensorFlow_HOME}/include
-    /opt/tensorflow/include
 )
+
+message(STATUS "Found TensorFlow Includes at: ${TensorFlow_INCLUDE_DIR}")
 
 find_library(TensorFlow_CC_LIBRARY
   NAMES tensorflow_cc
   HINTS
-    ${TensorFlow_HOME}/lib
+    ${TensorFlow_ROOT_DIR}/lib  
+    ${TensorFlow_HOME}/lib      
+    $ENV{TensorFlow_ROOT_DIR}/lib
     $ENV{TensorFlow_HOME}/lib
-    /opt/tensorflow/lib
 )
+
+message(STATUS "Found TensorFlow C Library at: ${TensorFlow_CC_LIBRARY}")
+
 
 find_library(TensorFlow_FRAMEWORK_LIBRARY
   NAMES tensorflow_framework
   HINTS
-    ${TensorFlow_HOME}/lib
+    ${TensorFlow_ROOT_DIR}/lib  
+    ${TensorFlow_HOME}/lib      
+    $ENV{TensorFlow_ROOT_DIR}/lib
     $ENV{TensorFlow_HOME}/lib
-    /opt/tensorflow/lib
 )
+
+message(STATUS "Found TensorFlow Framework Library at: ${TensorFlow_FRAMEWORK_LIBRARY}")
+
 
 # Find Protobuf 
 find_path(Protobuf_INCLUDE_DIR
   NAMES google/protobuf/message.h
   HINTS
+    ${Protobuf_ROOT_DIR}/include    
     ${Protobuf_HOME}/include
+    $ENV{Protobuf_ROOT_DIR}/include
     $ENV{Protobuf_HOME}/include
-    /opt/protobuf/include
 )
+
+message(STATUS "Found Protobuf Includes at: ${Protobuf_INCLUDE_DIR}")
+
 
 find_library(Protobuf_LIBRARY
   NAMES protobuf
   HINTS
+    ${Protobuf_ROOT_DIR}/lib    
     ${Protobuf_HOME}/lib
+    $ENV{Protobuf_ROOT_DIR}/lib
     $ENV{Protobuf_HOME}/lib
-    /opt/protobuf/lib
 )
+
+message(STATUS "Found Protobuf Library at: ${Protobuf_LIBRARY}")
 
 # Combine Results
 set(TensorFlow_INCLUDE_DIRS
